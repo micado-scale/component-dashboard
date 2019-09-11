ef6822dc09b0a94279c042adcf12be341a890617
maystery <emodi.mark@gmail.com>
2019-09-11 11:15:49 +0200

    feat: optimizer can be disable

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -32,6 +32,8 @@ def init_application(app):
         'MICADO_FRONTEND_IP', '127.0.0.1')
     app.config["MICADO_VERSION"] = os.getenv(
         'MICADO_VERSION', '------')
+    app.config["DISABLE_OPTIMIZER"] = os.getenv(
+        'DISABLE_OPTIMIZER', 'FALSE')
     app.register_blueprint(home)
     app.logger.info("{} application initialized using frontend IP {}" \
         .format(__name__, app.config["FRONTEND_IP"]))
--- dashboard/views/home.py
+++ dashboard/views/home.py
@@ -5,34 +5,35 @@ home = Blueprint("home", __name__, url_prefix="/")
 
 @home.route("/")
 @home.route("/dashboard")
-@home.route("/prometheus")
-@home.route("/dashboard/prometheus")
+
+@home.route("/kubernetes")
+@home.route("/dashboard/kubernetes")
 @register_menu(home, '.', "Home", order=0)
-@register_menu(home, '.prometheus', "Prometheus", order=2)
-def home_prometheus():
-    return render_template(['prometheus.html','base.html'],
-        frontend_ip=current_app.config['FRONTEND_IP'], version=current_app.config['MICADO_VERSION'])
+@register_menu(home, '.kubernetes', "Kubernetes", order=0)
+def home_kubernetes_dashboard():
+    return render_template(['kubernetes.html','base.html'],
+        frontend_ip=current_app.config['FRONTEND_IP'], version=current_app.config['MICADO_VERSION'], disable_optimizer=current_app.config['DISABLE_OPTIMIZER'])
 
 @home.route("/grafana")
 @home.route("/dashboard/grafana")
 @register_menu(home, '.grafana', "Grafana", order=1)
 def home_grafana():
     return render_template(['grafana.html','base.html'],
-        frontend_ip=current_app.config['FRONTEND_IP'], version=current_app.config['MICADO_VERSION'])
+        frontend_ip=current_app.config['FRONTEND_IP'], version=current_app.config['MICADO_VERSION'], disable_optimizer=current_app.config['DISABLE_OPTIMIZER'])
 
-@home.route("/kubernetes")
-@home.route("/dashboard/kubernetes")
-@register_menu(home, '.kubernetes', "Kubernetes", order=0)
-def home_kubernetes_dashboard():
-    return render_template(['kubernetes.html','base.html'],
-        frontend_ip=current_app.config['FRONTEND_IP'], version=current_app.config['MICADO_VERSION'])
+@home.route("/prometheus")
+@home.route("/dashboard/prometheus")
+@register_menu(home, '.prometheus', "Prometheus", order=2)
+def home_prometheus():
+    return render_template(['prometheus.html','base.html'],
+        frontend_ip=current_app.config['FRONTEND_IP'], version=current_app.config['MICADO_VERSION'], disable_optimizer=current_app.config['DISABLE_OPTIMIZER'])
 
 @home.route("/optimizer")
 @home.route("/dashboard/optimizer")
 @register_menu(home, '.optimizer', "Optimizer", order=3)
 def home_optimizer():
     return render_template(['optimizer.html','base.html'],
-        frontend_ip=current_app.config['FRONTEND_IP'], version=current_app.config['MICADO_VERSION'])
+        frontend_ip=current_app.config['FRONTEND_IP'], version=current_app.config['MICADO_VERSION'], disable_optimizer=current_app.config['DISABLE_OPTIMIZER'])
 
 @home.route("/logout")
 @register_menu(home, '.logout', "Log out", order=4)
